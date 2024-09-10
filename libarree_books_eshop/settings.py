c9e20ff3ecfd7af588fda70d605bbeff84d5bc30
naylafn <naylafarah.31@gmail.com>
2024-09-10 18:25:55 +0700

    debug statics

--- libarree_books_eshop/settings.py
+++ libarree_books_eshop/settings.py
@@ -122,7 +122,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'main' / 'static',
+# ]
+
+# # Menambahkan STATIC_ROOT untuk menyimpan file statis yang akan dikumpulkan
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
