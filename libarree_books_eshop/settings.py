a6ce8c2a9139dee3ecaf087eca9c8ff67611f6a0
naylafn <naylafarah.31@gmail.com>
2024-09-25 09:19:58 +0700

    tugas 4 add requirements.txt

--- libarree_books_eshop/settings.py
+++ libarree_books_eshop/settings.py
@@ -29,7 +29,6 @@ DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "nayla-farah-librarreebooks.pbp.cs.ui.ac.id"]
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     # main application
     'main',
 ]
