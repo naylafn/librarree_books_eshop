7fb85496f03d0dae7f754e14f4e33046921705d7
naylafn <naylafarah.31@gmail.com>
2024-10-09 07:11:40 +0700

    tugas 6 finish

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,17 @@
 from django.forms import ModelForm
 from main.models import Book
+from django.utils.html import strip_tags
+
 
 class BookEntryForm(ModelForm):
     class Meta:
         model = Book
         fields = ["name", "author", "price", "image"]
+
+        def clean_name(self):
+            name = self.cleaned_data["name"]
+            return strip_tags(name)
+
+        def clean_author(self):
+            author = self.cleaned_data["author"]
+            return strip_tags(author)
--- main/views.py
+++ main/views.py
@@ -13,15 +13,19 @@ from django.contrib.auth import authenticate, login, logout
 import datetime
 from django.urls import reverse
 
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
+
 # Create your views here.
 @login_required(login_url='/login')
 def show_main(request):
-    book_entries = Book.objects.filter(user=request.user)
+    # book_entries = Book.objects.filter(user=request.user)
     context = {
         'application' : 'Librarree',
         'name': request.user.username,
         'class': 'PBP F',
-        'book_entries': book_entries,
+        # 'book_entries': book_entries,
         'last_login': request.COOKIES['last_login'],
     }
     return render(request, "main.html", context)
@@ -39,11 +43,11 @@ def create_book_entry(request):
     return render(request, "create_book_entry.html", context)
 
 def show_xml(request):
-    data = Book.objects.all()
+    data = Book.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = Book.objects.all()
+    data = Book.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
@@ -76,6 +80,8 @@ def login_user(request):
         response = HttpResponseRedirect(reverse("main:show_main"))
         response.set_cookie('last_login', str(datetime.datetime.now()))
         return response
+      else:
+        messages.error(request, "Invalid username or password. Please try again.")
 
    else:
       form = AuthenticationForm(request)
@@ -106,3 +112,21 @@ def delete_book(request, id):
     book.delete()
     # Kembali ke halaman awal
     return HttpResponseRedirect(reverse('main:show_main'))
+
+@csrf_exempt
+@require_POST
+def add_book_entry_ajax(request):
+    name = strip_tags(request.POST.get("name"))
+    author = strip_tags(request.POST.get("author"))
+    price = request.POST.get("price")
+    image = request.POST.get("image")
+    user = request.user
+
+    new_book = Book(
+        name=name, author=author,
+        price=price, image=image,
+        user=user
+    )
+    new_book.save()
+
+    return HttpResponse(b"CREATED", status=201)
